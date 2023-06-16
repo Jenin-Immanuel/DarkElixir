f3880503118a5da61b6fdf9fa6587dec902522b5
Jenin <jeninimmi2003@gmail.com>
2023-06-16 11:39:24 +0530

    Tuple module functions done

--- src/Modules.java
+++ src/Modules.java
@@ -1,40 +1,105 @@
 public class Modules {
+
+    static void expect(RuntimeValueType given, RuntimeValueType expected, String msg) {
+        if(given != expected) {
+            System.err.println(msg);
+            System.exit(0);
+        }
+    }
     static void declareTupleModule(Environment scope) {
         RModule module = new RModule("Tuple");
 
         // at/2
         module.functions.put("at", RNativeFunction.MAKE_NATIVE_FN(((args, env) -> {
+            String argFormat = "InvalidArguments: Argument Format of Tuple.at/2 (tuple, index)";
             if(args.size() != 2) {
                 System.err.println(module.moduleName + " at function accepts only two argument");
                 System.exit(0);
             }
 
-            if(args.get(0).getKind() != RuntimeValueType.Tuple) {
-                System.err.println("Error");
-                System.exit(0);
-            }
-
-            if(args.get(1).getKind() != RuntimeValueType.Number) {
-                System.err.println("Error");
-                System.exit(0);
-            }
+            expect(args.get(0).getKind(), RuntimeValueType.Tuple, argFormat);
+            expect(args.get(1).getKind(), RuntimeValueType.Number, argFormat);
 
 
             var firstArg = (RTupleValue) args.get(0);
             var secondArg = (RNumberValue) args.get(1);
 
-            if (firstArg == null || firstArg.contents == null) {
-                throw new RuntimeException("Error: Invalid first argument");
-            }
 
-            int index = secondArg.number.intValue();
-            if (index >= firstArg.contents.size()) {
-                System.err.println("Error: Index out of bounds");
+            return firstArg.contents.get(secondArg.number.intValue());
+        })));
+
+        // append/2
+        module.functions.put("append", RNativeFunction.MAKE_NATIVE_FN(((args, env) -> {
+            String argError = "InvalidArguments: Argument Format of Tuple.append/2 (tuple, element)";
+            if(args.size() != 2) {
+                System.err.println(module.moduleName + " at function accepts only two argument");
                 System.exit(0);
             }
 
-            return firstArg.contents.get(index);
+            expect(args.get(0).getKind(), RuntimeValueType.Tuple, argError);
+            var firstArg = (RTupleValue) args.get(0);
+            firstArg.contents.add(args.get(1));
+            return firstArg;
         })));
+
+        // delete_at/2
+        module.functions.put("delete_at", RNativeFunction.MAKE_NATIVE_FN(((args, env) -> {
+            String argError = "InvalidArguments: Argument Format of Tuple.delete_at/2 (tuple, index)";
+            if(args.size() != 2) {
+                System.err.println(module.moduleName + " at function accepts only two argument");
+                System.exit(0);
+            }
+
+            expect(args.get(0).getKind(), RuntimeValueType.Tuple, argError);
+            expect(args.get(1).getKind(), RuntimeValueType.Number, argError);
+
+
+            var firstArg = (RTupleValue) args.get(0);
+            var secondArg = (RNumberValue) args.get(1);
+            firstArg.contents.remove(secondArg.number.intValue());
+            return firstArg;
+        })));
+
+        // insert_at/3
+        module.functions.put("insert_at", RNativeFunction.MAKE_NATIVE_FN(((args, env) -> {
+            String argError = "InvalidArguments: Argument Format of Tuple.insert_at/3 (tuple, index, value)";
+            if(args.size() != 3) {
+                System.err.println(module.moduleName + " at function accepts only two argument");
+                System.exit(0);
+            }
+
+            expect(args.get(0).getKind(), RuntimeValueType.Tuple, argError);
+            expect(args.get(1).getKind(), RuntimeValueType.Number, argError);
+
+
+            var firstArg = (RTupleValue) args.get(0);
+            var secondArg = (RNumberValue) args.get(1);
+
+            if(secondArg.number >= firstArg.contents.size()) {
+                System.err.println("Invalid Argument:Size of the tuple is smaller than the given index. Tuple.insert_at/3");
+            }
+            System.out.println(secondArg.number.intValue() + " " + firstArg.contents.size());
+            firstArg.contents.add(secondArg.number.intValue(), args.get(2));
+            return firstArg;
+        })));
+
+        // to_list/1
+        module.functions.put("to_list", RNativeFunction.MAKE_NATIVE_FN(((args, env) -> {
+            String argError = "InvalidArguments: Argument Format of Tuple.to_list/1 (tuple)";
+
+            if(args.size() != 1) {
+                System.err.println(module.moduleName + " to_list function accepts only one argument");
+                System.exit(0);
+            }
+
+            expect(args.get(0).getKind(), RuntimeValueType.Tuple, argError);
+            var firstArg = (RTupleValue) args.get(0);
+
+            RListValue list = new RListValue();
+            list.contents.addAll(firstArg.contents);
+            return list;
+        })));
+
 
         scope.declareVariable("Tuple", module, true);
     }
